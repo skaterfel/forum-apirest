f951bae3f8082fe6c2235f46bb2218ddbd1ede31
Felipe Melo <felipe.leandro2@gmail.com>
2022-08-03 23:28:10 -0300

    protejento 'moda antiga' com SpringSecurity

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebMvcSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
+    // configura a autenticação login e acessos
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+
+    }
+
+    // config de autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+    }
+
+    // condif de recursos estaticos js, css, imagens
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
+}
--- src/main/java/br/com/alura/forum/controller/HelloController.java
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -1,7 +1,6 @@
 package br.com.alura.forum.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
