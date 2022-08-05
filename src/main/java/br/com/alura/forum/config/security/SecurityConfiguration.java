3526e550e95ee4b25447c6b4217e380dc40c25a7
Felipe Melo <felipe.leandro2@gmail.com>
2022-08-05 09:23:14 -0300

    Finalizando a app e criando a DOC com Swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -59,7 +59,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // condif de recursos estaticos js, css, imagens
     @Override
     public void configure(WebSecurity web) throws Exception {
-
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**",
+                "/swagger-resources/**");
     }
 
 }
